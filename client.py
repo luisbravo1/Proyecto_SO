d60b504e43b0d3776163a1e6f634b6c346040b7a
Luis Bravo <luis.g.bravo1@gmail.com>
2018-12-02 19:52:38 -0600

    First commit

---
+++ client.py
@@ -0,0 +1,36 @@
+import socket
+import sys
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#
+# Connect the socket to the port where the server is listening
+server_address = ('localhost', 10000)
+print >>sys.stderr, 'connecting to %s port %s' % server_address
+sock.connect(server_address)
+
+# After the connection is established, data can be sent through the socket with sendall() and received with recv(), just as in the server.
+
+messages = ['create // process A', 'create //process B']
+try:
+    
+    # Send data
+    for m in messages:
+		print >>sys.stderr, 'client sending "%s"' % m
+		sock.sendall(m)
+
+		# Look for the response
+    
+		respuesta = sock.recv(256)
+        
+		print >>sys.stderr, 'client received "%s"' % respuesta
+
+finally:
+    print >>sys.stderr, 'closing socket'
+    sock.close()
+
+  
+
+
+def main(args):
+    return 0
